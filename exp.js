10285ce6d4dc8836bc7a4135a89514d0ab705199
Heejin Park <psc1987@naver.com>
2020-09-28 17:13:28 +0900

    Add files via upload

--- exp.js
+++ exp.js
@@ -5,6 +5,7 @@
 // 서버를 요청 대기 상태로 만들수 있다.
 
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 
@@ -21,11 +22,46 @@ function logger2(req, res, next) {
     next(); // 다음 호출을 수행
 }
 
-app.use(logger);
+app.use(logger); // use를 사용해 호출
 app.use(logger2);
+app.use(moargan('dev'));
+
+
+// 일반 미들웨어 VS 에러 미들웨어
+function commonmw(req, res, next) {
+    console.log('commonmw');
+    next(new Error('error occured')); // 에러 객체 넘겨줌
+}
+
+function errormw(err, req, res, next) { // 에러 객체를 첫번째 파라미터로 받아서 메세지 출력
+    console.log(err.message);
+    next();
+}
+
+app.use(commonmw);
+app.use(errormw);
 
 
 
-app.listen(3000, function() {
+// 3. 라우팅
+// 요청 url에 대해 적절한 핸들러 함수로 연결해 주는 기능
+// 어플리케이션의 get(), post() 메소드로 구현할 수 있다.
+// 라우팅을 위한 Router 클래스 사용 가능
+
+
+
+// 4. 요청 객체
+// 클라이언트 요청 정보를 담은 객체를 요청(Request)객체라고 한다.
+// req.params(), req.query(), req.body() 메소드를 주로 사용 한다.
+
+
+// 5. 응답객체
+// 클라이언트 응답 정보를 담은 객체를 응답(Response)객체라고 한다.
+// req.send(), req.status(), req.json() 메소드를 주로 사용 한다.
+
+
+
+
+/*app.listen(3000, function() {
     console.log('Server is running');
-})
+})*/
